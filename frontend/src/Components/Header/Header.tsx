dc9ddfb8bead34d232bb4c610dd360b574f3f745
Koitchev,Ivan I.G. <ivan@appsemble.com>
2026-01-14 21:18:32 +0200

    Update deps

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import './Header.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 function Header() {
-  return <div className="header-container">Title: Interesno.info</div>;
+  return (
+    <AppBar position="static" color="primary">
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Typography variant="h6" component="div">
+            Interesno.info
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Header;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
